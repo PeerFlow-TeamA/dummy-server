ddcdcd382ee2fe77824dae559763dc8eb5e85482
Two-Jay <djeeee1272@gmail.com>
2023-07-08 09:06:15 +0900

    feat: 엔티티 정의 완료

--- dummy-server/name/handlers/entity.py
+++ dummy-server/name/handlers/entity.py
@@ -12,3 +12,151 @@ class Writter():
             "nickname": self.nickname,
             "password": self.password
         }
+    
+
+class Question():
+    def __init__(self, 
+                id,
+                title,
+                content,
+                nickname,
+                password,
+                views,
+                recomment,
+                created_at,
+                updated_at):
+        self.id : int = id
+        self.title : str = title
+        self.content : str = content
+        self.nickname : str = nickname
+        self.password : str = password
+        self.views : int = views
+        self.recomment : int = recomment
+        self.created_at : str = created_at
+        self.updated_at : str = updated_at
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "content": self.content,
+            "nickname": self.nickname,
+            "password": self.password,
+            "views": self.views,
+            "recomment": self.recomment,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
+
+class Answer():
+    def __init__(self,
+                id,
+                question_id,
+                content,
+                nickname,
+                password,
+                recomment,
+                isAdopted,
+                created_at,
+                updated_at):
+        self.id : int = id
+        self.question_id : int = question_id
+        self.content : str = content
+        self.nickname : str = nickname
+        self.password : str = password
+        self.recomment : int = recomment
+        self.isAdopted : bool = isAdopted
+        self.created_at : str = created_at
+        self.updated_at : str = updated_at
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "question_id": self.question_id,
+            "content": self.content,
+            "nickname": self.nickname,
+            "password": self.password,
+            "recomment": self.recomment,
+            "isAdopted": self.isAdopted,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
+    
+class QuestionComment():
+    def __init__(self,
+                id,
+                question_id,
+                content,
+                nickname,
+                password,
+                created_at,
+                updated_at):
+        self.id : int = id
+        self.question_id : int = question_id
+        self.content : str = content
+        self.nickname : str = nickname
+        self.password : str = password
+        self.created_at : str = created_at
+        self.updated_at : str = updated_at
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "question_id": self.question_id,
+            "content": self.content,
+            "nickname": self.nickname,
+            "password": self.password,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
+    
+class AnswerComment():
+    def __init__(self,
+                id,
+                answer_id,
+                content,
+                nickname,
+                password,
+                created_at,
+                updated_at):
+        self.id : int = id
+        self.answer_id : int = answer_id
+        self.content : str = content
+        self.nickname : str = nickname
+        self.password : str = password
+        self.created_at : str = created_at
+        self.updated_at : str = updated_at
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "answer_id": self.answer_id,
+            "content": self.content,
+            "nickname": self.nickname,
+            "password": self.password,
+            "created_at": self.created_at,
+            "updated_at": self.updated_at
+        }
+    
+class Pageable():
+    def __init__(self, sort, pageNumber, pageSize, offset, paged, unpaged):
+        self.sort : dict = {
+            "sorted": False,
+            "unsorted": True,
+            "empty": True
+        }
+        self.pageNumber : int = 0
+        self.pageSize : int = 0
+        self.offset : int = 0
+        self.paged : bool = False
+        self.unpaged : bool = False
+
+    def to_dict(self):
+        return {
+            "sort": self.sort,
+            "pageNumber": self.pageNumber,
+            "pageSize": self.pageSize,
+            "offset": self.offset,
+            "paged": self.paged,
+            "unpaged": self.unpaged
+        }
