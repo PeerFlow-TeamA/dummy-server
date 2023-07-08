5d27a398001b5d0e991f16d037ad0e65e242c23b
Two-Jay <djeeee1272@gmail.com>
2023-07-09 05:14:18 +0900

    feat: QuestionNotFoundError, PasswordIncorrectError 관련 Exception class 추가

--- dummy-server/name/handlers/exceptions.py
+++ dummy-server/name/handlers/exceptions.py
@@ -1,11 +1,29 @@
+error_msg_key = "message"
+sort_value_error_msg = "sort standard incorrected - sort standard must be one of [lastest, views, recommends]"
+category_value_error_msg = "category not correct - category must be one of [ft_irc, minishell, minirt]"
+question_not_found_error_msg = "Question not found"
+password_incorrect_error_msg = "Password incorrect"
+
 class SortValueError(Exception):
-    def __init__(self, message):
-        self.message = message
+    def __init__(self):
+        self.message = sort_value_error_msg
     def __str__(self):
         return self.message
     
 class CategoryValueError(Exception):
-    def __init__(self, message):
-        self.message = message
+    def __init__(self):
+        self.message = category_value_error_msg
+    def __str__(self):
+        return self.message
+    
+class QuestionNotFoundError(Exception):
+    def __init__(self):
+        self.message = question_not_found_error_msg
+    def __str__(self):
+        return self.message
+    
+class PasswordIncorrectError(Exception):
+    def __init__(self):
+        self.message = password_incorrect_error_msg
     def __str__(self):
         return self.message
