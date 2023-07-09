b08cad186cb02acc889b234f8f33ae12a4829717
Two-Jay <djeeee1272@gmail.com>
2023-07-09 22:26:15 +0900

    feat:status code 클래스 구현

--- dummy-server/name/handlers/http_handler.py
+++ dummy-server/name/handlers/http_handler.py
@@ -9,6 +9,20 @@ class HTTP_METHOD():
     DELETE = "DELETE"
     PATCH = "PATCH"
 
+class HTTP_STATUS_CODE():
+    # 2xx
+    OK = 200
+    CREATED = 201
+    ACCEPTED = 202
+    NO_CONTENT = 204
+    # 4xx
+    BAD_REQUEST = 400
+    UNAUTHORIZED = 401
+    FORBIDDEN = 403
+    NOT_FOUND = 404
+    # 5xx
+    INTERNAL_SERVER_ERROR = 500
+
 def error_response(status_code, message_key, message):
     return JsonResponse({
         message_key : message
