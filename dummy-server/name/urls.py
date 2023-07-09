0fb2ae89bdd6ae4fdb4c06eeb2c1b6587b09236c
Two-Jay <djeeee1272@gmail.com>
2023-07-09 23:23:52 +0900

    feat : recommendation url path에 추가

--- dummy-server/name/urls.py
+++ dummy-server/name/urls.py
@@ -26,11 +26,13 @@ urlpatterns = [
     path("v1/search", views.A_MAI_01_handler),
     path("v1/question/<int:question_id>/comment/<int:comment_id>", views.C_DET_question_comment_handler),
     path("v1/question/<int:question_id>/comment", views.C_DET_question_comment_handler),
+    path("v1/question/<int:question_id>/recommendation", views.C_DET_question_handler),
     path("v1/question/<int:question_id>", views.C_DET_question_handler),
     path("v1/question", views.B_WRI_question_handler),
     path("v1/answer", views.C_DET_answer_handler),
     path("v1/answer/<int:answer_id>/comment/<int:comment_id>", views.C_DET_answer_comment_handler),
     path("v1/answer/<int:answer_id>/comment", views.C_DET_answer_comment_handler),
+    path("v1/answer/<int:answer_id>/recommendation", views.C_DET_answer_handler),
     path("v1/answer/<int:question_id>", views.C_DET_answer_handler),
 
 
