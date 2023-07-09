5e2041f3582d671fc2b1040c609de92c1ffa875a
Two-Jay <djeeee1272@gmail.com>
2023-07-09 18:45:10 +0900

    feat: C_DET answer 핸들러 구현

--- dummy-server/name/handlers/C_DET.py
+++ dummy-server/name/handlers/C_DET.py
@@ -1,1 +1,3 @@
 from .C_DET_question import *
+from .C_DET_answer import *
+from .C_DET_question_comment import *
--- dummy-server/name/handlers/C_DET_answer.py
+++ dummy-server/name/handlers/C_DET_answer.py
@@ -9,7 +9,6 @@ from .exceptions import *
 def C_DET_01_create_answer(request):
     try:
         body_params = json.loads(request.body.decode("utf-8"))
-        id = body_params['id']
         question_id = body_params['question_id']
         content = body_params['content']
         nickname = body_params['nickname']
@@ -22,7 +21,7 @@ def C_DET_01_create_answer(request):
             raise QuestionNotFoundError()
 
         elem = Answer(
-            id = id,
+            id = get_datapool().get_next_id(get_datapool().ANSWER_LIST),
             question_id = question_id,
             content = content,
             nickname = nickname,
@@ -39,12 +38,116 @@ def C_DET_01_create_answer(request):
             "message" : "answer created successfully"
         })
     except QuestionNotFoundError as e:
-        return error_response(400, error_msg_key, question_not_found_error_msg)
+        return error_response(404, error_msg_key, question_not_found_error_msg)
+    except Exception as e:
+        return error_response(500, error_msg_key, "Error occured - " + str(e))
+    
+def C_DET_02_modify_answer(request):
+    try:
+        body_params = json.loads(request.body.decode("utf-8"))
+        question_id = body_params['question_id']
+        content = body_params['content']
+        nickname = body_params['nickname']
+        password = body_params['password']
+        created_at = body_params['created_at']
+
+        found = get_datapool().get_question_by_id(question_id)
+
+        if found is None:
+            raise QuestionNotFoundError()
+        if found.password != password:
+            raise PasswordIncorrectError()
+
+        elem = Answer(
+            id = get_datapool().get_next_id(get_datapool().ANSWER_LIST),
+            question_id = question_id,
+            content = content,
+            nickname = nickname,
+            password = password,
+            recomment = 0,
+            isAdopted = False,
+            created_at = found.created_at,
+            updated_at = created_at
+        )
+
+        get_datapool().ANSWER_LIST.append(elem)
+
+        return normal_response_json({
+            "message" : "answer created successfully"
+        }, 201)
+    except QuestionNotFoundError as e:
+        return error_response(404, error_msg_key, e.message)
+    except PasswordIncorrectError as e:
+        return error_response(403, error_msg_key, e.message)
+    except Exception as e:
+        return error_response(500, error_msg_key, "Error occured - " + str(e))
+    
+def C_DET_03_delete_answer(request, answer_id):
+    try:
+        body_params = json.loads(request.body.decode("utf-8"))
+        password = body_params['password']
+
+
+        found = get_datapool().get_by_id(get_datapool().ANSWER_LIST, answer_id)
+
+        if found is None:
+            raise AnswerNotFoundError()
+        if found.password != password:
+            raise PasswordIncorrectError()
+
+        get_datapool().delete(get_datapool().ANSWER_LIST, found)
+
+        return normal_response_json({
+            "message" : "answer deleted successfully"
+        })
+    except PasswordIncorrectError as e:
+        return error_response(403, error_msg_key, e.message)
+    except AnswerNotFoundError as e:
+        return error_response(404, error_msg_key, e.message)
     except Exception as e:
         return error_response(500, error_msg_key, "Error occured - " + str(e))
 
-def C_DET_answer_handler(request, question_id = None):
+def C_DET_04_adopt_answer(request, answer_id):
+    try:
+        body_params = json.loads(request.body.decode("utf-8"))
+        password = body_params['password']
+
+        found = get_datapool().get_by_id(get_datapool().ANSWER_LIST, answer_id)
+        parent_question = get_datapool().get_by_id(get_datapool().QUESTION_LIST, answer_id)
+
+        if found is None:
+            raise AnswerNotFoundError()
+        if parent_question is None:
+            raise QuestionNotFoundError()
+        if parent_question.password != password:
+            raise PasswordIncorrectError()
+
+        found.isAdopted = True
+
+        return normal_response_json({
+            "message" : "answer adopted successfully"
+        })
+    except QuestionNotFoundError as e:
+        return error_response(404, error_msg_key, e.message)
+    except PasswordIncorrectError as e:
+        return error_response(403, error_msg_key, e.message)
+    except AnswerNotFoundError as e:
+        return error_response(404, error_msg_key, e.message)
+    except Exception as e:
+        return error_response(500, error_msg_key, "Error occured - " + str(e))
+
+@csrf_exempt
+def C_DET_answer_handler(request, answer_id = None):
+    if request.method == HTTP_METHOD.POST and answer_id is not None:
+        return C_DET_03_delete_answer(request, answer_id)
+    
     if request.method == HTTP_METHOD.POST:
         return C_DET_01_create_answer(request)
     
+    if request.method == HTTP_METHOD.PUT:
+        return C_DET_02_modify_answer(request, answer_id)
+    
+    if request.method == HTTP_METHOD.PATCH and answer_id is not None:
+        return C_DET_04_adopt_answer(request, answer_id)
+
     return not_allowed_method_response()
